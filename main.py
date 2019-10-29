169ead295cae27b7f9904b373b59da471137e23d
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2019-10-29 13:58:14 +0100

    Cut face to eyes

--- main.py
+++ main.py
@@ -13,10 +13,18 @@ while True:
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
         gray_face = gray_frame[y:y+h, x:x+w] 
         color_face = frame[y:y+h, x:x+w] 
+
+        # Cut face to eyes
+        faceHeight = gray_face.shape[0]
+
+        cut_face = gray_face[int(faceHeight/4):int(faceHeight/1.7), :]
+        _, cut_face = cv2.threshold(cut_face, 127, 255, cv2.THRESH_BINARY)
 
         # Detect eyes
         eyes = eye_cascade.detectMultiScale(gray_face)
@@ -26,6 +34,9 @@ while True:
                 cv2.rectangle(color_face, (ex, ey), (ex+ew, ey+eh), (0, 225, 255), 2)
 
     cv2.imshow("Camera", frame)
+
+    cv2.imshow("Eyes", cut_face)
+
     k = cv2.waitKey(10)
     if k == ESC & 0xff:
         break
