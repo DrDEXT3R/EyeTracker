12171e6c46bce6f171aecd8ca146f759b76b1238
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2019-10-30 22:19:04 +0100

    Code refactoring

--- main.py
+++ main.py
@@ -1,77 +1,87 @@
 import cv2
 import numpy as np
 
-ESC = 27
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-cap = cv2.VideoCapture(0)
-
-
-detector_params = cv2.SimpleBlobDetector_Params()
-detector_params.filterByArea = True
-detector_params.maxArea = 1500
-detector = cv2.SimpleBlobDetector_create(detector_params)
-
-
-
-def blob_process(img, threshold, detector):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
-    # img = cv2.erode(img, None, iterations=2)
-    # img = cv2.dilate(img, None, iterations=4)
-    # img = cv2.medianBlur(img, 5)
-    keypoints = detector.detect(img)
-    # print(keypoints)
-    return keypoints
-
 
 def nothing(x):
     pass
 
+
+def threshold_setting(img):
+    img_height = img.shape[0]
+    img_to_set_up = img[int(img_height/4):int(img_height/1.7), :]
+
+    value_from_trackbar = cv2.getTrackbarPos('threshold', 'Settings')
+    _, img_to_set_up = cv2.threshold(img_to_set_up, value_from_trackbar, 255, cv2.THRESH_BINARY)
+    return value_from_trackbar, img_to_set_up
+
+
+def blob_detection(img, t):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(gray_img, t, 255, cv2.THRESH_BINARY)
+
+    img = cv2.erode(img, None, iterations=2)
+    img = cv2.dilate(img, None, iterations=4)
+    img = cv2.medianBlur(img, 5)
+
+    points = detector.detect(img)
+    return points
+
+
+## Set up blob detector
+params = cv2.SimpleBlobDetector_Params()
+params.filterByArea = True
+params.maxArea = 1600
+detector = cv2.SimpleBlobDetector_create(params)
+
+
+ESC = 27
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+
 cv2.namedWindow('Settings')
 cv2.createTrackbar('threshold', 'Settings', 50, 255, nothing)
+
+
+cap = cv2.VideoCapture(0)
+
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
     for (x, y, w, h) in faces:
-        #cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # Mark face
 
         gray_face = gray_frame[y:y+h, x:x+w] 
         color_face = frame[y:y+h, x:x+w] 
 
-        # Cut face to eyes
-        faceHeight = gray_face.shape[0]
-
-        cut_face = gray_face[int(faceHeight/4):int(faceHeight/1.7), :]
-        threshold = cv2.getTrackbarPos('threshold', 'Settings')
-        _, cut_face = cv2.threshold(cut_face, threshold, 255, cv2.THRESH_BINARY)
-
+        threshold, eyes_to_set_up = threshold_setting(gray_face)
+  
         # Detect eyes
         eyes = eye_cascade.detectMultiScale(gray_face)
         for (ex, ey, ew, eh) in eyes:
-            face_height = np.size(color_face, 0)  
+            face_height = np.size(color_face, 0) 
 
-            if ey < face_height / 2:
-                #cv2.rectangle(color_face, (ex, ey), (ex+ew, ey+eh), (0, 225, 255), 2)
-                eye = color_face[int(ey+ey/4):ey+eh, ex:ex+ew]
+            # Prevent false detection 
+            if ey < (face_height / 2): 
+                #cv2.rectangle(color_face, (ex, ey), (ex+ew, ey+eh), (0, 225, 255), 2) # Mark eye
+                eye = color_face[ey:ey+eh, ex:ex+ew]
 
-                keypoints = blob_process(eye, threshold, detector)
-                eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
+                keypoints = blob_detection(eye, threshold)
+                eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     cv2.imshow("Camera", frame)
 
-    if 'cut_face' in globals():
-        cv2.imshow("Settings", cut_face)
+    if 'eyes_to_set_up' in globals():
+        cv2.imshow("Settings", eyes_to_set_up)
 
-    if 'eye' in globals():
-        cv2.imshow("Eye", eye)
+    # if 'eye' in globals():
+    #     cv2.imshow("Eye", eye)
 
     k = cv2.waitKey(10)
     if k == ESC & 0xff:
